90fe722db716fd1f1766a006bc086dca304c1fbc
Houman Sanati <houmansnt@gmail.com>
2022-12-27 16:08:58 +0330

    add some tests for output and input validation

--- test/wallet/BitcoinWallet.test.ts
+++ test/wallet/BitcoinWallet.test.ts
@@ -99,7 +99,7 @@ describe('BitcoinWallet test', () => {
         });
     });
 
-    describe('Test with one UTXO with exact amount', () => {
+    describe('Test with one UTXO ', () => {
         beforeEach(() => {
             mockApiResponse(accountByXpubUrl(testXpub1), AccountDiscoveryResponseWithOneUTXO);
             mockApiResponse(accountByXpubUrl(testXpub2), AccountDiscoveryResponseWithNoUTXO);
@@ -108,45 +108,85 @@ describe('BitcoinWallet test', () => {
             mockApiResponse(feeEstimationUrl(6), FeeEstimation6);
         });
 
-        it('Should return account with balance', async () => {
-            const result = await btcWallet.StartDiscovery();
-            expect(result.accounts?.length).to.equal(1);
-            if (result.accounts) {
-                expect(result.accounts[0].balance).to.equal(AccountDiscoveryResponseWithOneUTXO.balance);
-            } else expect.fail();
+        describe('Send the exact amount', () => {
+            it('Should return account with balance', async () => {
+                const result = await btcWallet.StartDiscovery();
+                expect(result.accounts?.length).to.equal(1);
+                if (result.accounts) {
+                    expect(result.accounts[0].balance).to.equal(AccountDiscoveryResponseWithOneUTXO.balance);
+                } else expect.fail();
+            });
+
+            it('Should return `No sufficient balance in your account` because tx amount equals to previous utxo amount and there nothing left for fee', async () => {
+                mockApiResponse(accountUtxoUrl, OneUTXO);
+
+                await btcWallet.StartDiscovery();
+                await expect(
+                    btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: parseInt(OneUTXO[0].value) }])
+                ).to.be.rejectedWith('No sufficient balance in your account');
+            });
+
+            it('Should generate transaction with one output and one input', async () => {
+                mockApiResponse(accountUtxoUrl, OneUTXO);
+
+                await btcWallet.StartDiscovery();
+                const rawTx = await btcWallet.GenerateTransaction([
+                    { Address: testBip49Address2, value: +OneUTXO[0].value - 3414 },
+                ]);
+                expect(rawTx.outputs.length).equals(1);
+                expect(rawTx.inputs.length).equals(1);
+            });
+
+            it('Should return error because send amount is more than overall balance', async () => {
+                mockApiResponse(accountUtxoUrl, OneUTXO);
+
+                await btcWallet.StartDiscovery();
+                await expect(
+                    btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: +OneUTXO[0].value + 1000 }])
+                ).to.be.rejectedWith('No sufficient balance in your account');
+            });
+
+            it('Output amount should be less than the the account balance (fee)', async () => {
+                mockApiResponse(accountUtxoUrl, OneUTXO);
+
+                const wallet = await btcWallet.StartDiscovery();
+                const rawTx = await btcWallet.GenerateTransaction([
+                    { Address: testBip49Address2, value: +OneUTXO[0].value - 3414 },
+                ]);
+
+                if (wallet.accounts) {
+                    expect(parseInt(rawTx.outputs[0].amount)).to.be.lessThan(parseInt(wallet!.accounts[0].balance));
+                } else expect.fail();
+            });
         });
 
-        it('Should return `No sufficient balance in your account` because tx amount equals to previous utxo amount and there nothing left for fee', async () => {
-            mockApiResponse(accountUtxoUrl, OneUTXO);
+        describe('Send half the balance', () => {
+            it('Should generate transaction with two outputs and one input', async () => {
+                mockApiResponse(accountUtxoUrl, OneUTXO);
 
-            await btcWallet.StartDiscovery();
-            await expect(
-                btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: parseInt(OneUTXO[0].value) }])
-            ).to.be.rejectedWith('No sufficient balance in your account');
-        });
+                await btcWallet.StartDiscovery();
+                const rawTx = await btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: 100000 }]);
+                expect(rawTx.outputs.length).equals(2);
+                expect(rawTx.inputs.length).equals(1);
+            });
 
-        it('Should generate transaction with one output and one input', async () => {
-            mockApiResponse(accountUtxoUrl, OneUTXO);
+            it('Outputs amount sum should be less than the account balance (fee)', async () => {
+                mockApiResponse(accountUtxoUrl, OneUTXO);
 
-            await btcWallet.StartDiscovery();
-            const rawTx = await btcWallet.GenerateTransaction([
-                { Address: testBip49Address2, value: +OneUTXO[0].value - 3414 },
-            ]);
-            expect(rawTx.outputs.length).equals(1);
-            expect(rawTx.inputs.length).equals(1);
-        });
-
-        it('Should return error because send amount is more than overall balance', async () => {
-            mockApiResponse(accountUtxoUrl, OneUTXO);
-
-            await btcWallet.StartDiscovery();
-            await expect(
-                btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: +OneUTXO[0].value + 1000 }])
-            ).to.be.rejectedWith('No sufficient balance in your account');
+                const wallet = await btcWallet.StartDiscovery();
+                const rawTx = await btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: 10000 }]);
+                let outputAmountsSum = 0;
+                rawTx.outputs.forEach((item) => {
+                    outputAmountsSum += parseInt(item.amount);
+                });
+                if (wallet.accounts) {
+                    expect(outputAmountsSum).to.be.lessThan(parseInt(wallet!.accounts[0].balance));
+                } else expect.fail();
+            });
         });
     });
 
-    describe('Several UTXO + ONE UTXO with exact same amount to send + fee', () => {
+    describe('Test with two UTXOs', () => {
         beforeEach(() => {
             mockApiResponse(accountByXpubUrl(testXpub1), AccountDiscoveryResponseWithTwoUTXO);
             mockApiResponse(accountByXpubUrl(testXpub2), AccountDiscoveryResponseWithNoUTXO);
@@ -163,23 +203,66 @@ describe('BitcoinWallet test', () => {
             } else expect.fail();
         });
 
-        it('Should generate transaction with one output and one input', async () => {
-            mockApiResponse(accountUtxoUrl, TwoUTXOs);
+        describe('send the exact amount', () => {
+            it('Should generate transaction with one output and one input', async () => {
+                mockApiResponse(accountUtxoUrl, TwoUTXOs);
 
-            await btcWallet.StartDiscovery();
-            const amount = +TwoUTXOs[0].value + +TwoUTXOs[1].value - 5782;
-            const rawTx = await btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: amount }]);
-            expect(rawTx.outputs.length).equals(1);
+                await btcWallet.StartDiscovery();
+                const amount = +TwoUTXOs[0].value + +TwoUTXOs[1].value - 5782;
+                const rawTx = await btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: amount }]);
+                expect(rawTx.outputs.length).equals(1);
+            });
+
+            it('Output amount should be less than account balance', async () => {
+                mockApiResponse(accountUtxoUrl, TwoUTXOs);
+
+                const wallet = await btcWallet.StartDiscovery();
+                const amount = +TwoUTXOs[0].value + +TwoUTXOs[1].value - 5782;
+                const rawTx = await btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: amount }]);
+                if (wallet.accounts) {
+                    expect(parseInt(rawTx.outputs[0].amount)).to.be.lessThan(parseInt(wallet.accounts[0].balance));
+                } else expect.fail();
+            });
         });
 
-        it('Should generate transaction with two outputs and two inputs', async () => {
-            mockApiResponse(accountUtxoUrl, TwoUTXOs);
+        describe('Send less than exact amount', () => {
+            it('Should generate transaction with two outputs and two inputs', async () => {
+                mockApiResponse(accountUtxoUrl, TwoUTXOs);
 
-            await btcWallet.StartDiscovery();
-            const amount = 300_000;
-            const rawTx = await btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: amount }]);
-            expect(rawTx.outputs.length).equals(2);
-            expect(rawTx.inputs.length).equals(2);
+                await btcWallet.StartDiscovery();
+                const amount = 300_000;
+                const rawTx = await btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: amount }]);
+                expect(rawTx.outputs.length).equals(2);
+                expect(rawTx.inputs.length).equals(2);
+            });
+
+            it('Inputs amount sum should equal account balance', async () => {
+                mockApiResponse(accountUtxoUrl, TwoUTXOs);
+
+                const wallet = await btcWallet.StartDiscovery();
+                const amount = 300_000;
+                const rawTx = await btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: amount }]);
+                let inputsSum = 0;
+                rawTx.inputs.forEach((item) => {
+                    inputsSum += parseInt(item!.amount!);
+                });
+                if (wallet.accounts) {
+                    expect(inputsSum).to.equal(parseInt(wallet.accounts[0].balance));
+                } else expect.fail();
+            });
+
+            it('Output amount should be less than account balance', async () => {
+                mockApiResponse(accountUtxoUrl, TwoUTXOs);
+
+                const wallet = await btcWallet.StartDiscovery();
+                const amount = 300_000;
+                const rawTx = await btcWallet.GenerateTransaction([{ Address: testBip49Address2, value: amount }]);
+                if (wallet.accounts) {
+                    expect(parseInt(rawTx.outputs[0].amount)).to.be.lessThan(
+                        parseInt(AccountDiscoveryResponseWithTwoUTXO.balance)
+                    );
+                } else expect.fail();
+            });
         });
     });
 });
